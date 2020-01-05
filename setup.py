733929a4420c3e1b417dbe5e09d097a1b5a8a5e5
Tobias Litherland <tobias.litherland@statkraft.com>
2020-01-05 22:39:42 +0100

    given up travis build, shyft is missing liblas on travis ubuntu

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='weather-tobiasli',
-                 version='1.0.0',
+                 version='1.0.1',
                  description='Tools for managing weather data using shyft.',
                  author='Tobias Litherland',
                  author_email='tobiaslland@gmail.com',
@@ -18,5 +18,5 @@ setuptools.setup(name='weather-tobiasli',
                      "License :: OSI Approved :: MIT License",
                      "Operating System :: OS Independent",
                  ],
-                 install_requires=['tregex-tobiasli', 'lnetatmo', 'pytest', 'numpy', 'cryptography', 'liblas']
+                 install_requires=['tregex-tobiasli', 'lnetatmo', 'pytest', 'numpy', 'cryptography']
                  )
