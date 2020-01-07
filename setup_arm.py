da3fc6500ee73064d7ac798c02acb7cb76250cea
Tobias Litherland <tobias.litherland@statkraft.com>
2020-01-07 22:36:31 +0100

    Added setup.py that works for ARM-architecture

---
+++ setup_arm.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='weather-tobiasli',
+                 version='1.0.1',
+                 description='Tools for managing weather data using shyft.',
+                 author='Tobias Litherland',
+                 author_email='tobiaslland@gmail.com',
+                 url='https://github.com/tobiasli/weather',
+                 packages=setuptools.find_packages(),
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                 ],
+                 install_requires=['tregex-tobiasli', 'lnetatmo', 'pytest', 'numpy', 'python-cryptography']
+                 )
