935f358ff64ede89ffcebeeb95a373a0b56a8ee0
Tobias Litherland <tobias.litherland@statkraft.com>
2020-01-12 21:03:26 +0100

    new version for dtss config fix

--- setup_arm.py
+++ setup_arm.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 print('INFO: For the package to work on ArchLinuxArm, install package python-cryptography separately from AUR.')
 
 setuptools.setup(name='weather-tobiasli',
-                 version='1.0.9',
+                 version='1.0.10',
                  description='Tools for managing weather data using shyft.',
                  author='Tobias Litherland',
                  author_email='tobiaslland@gmail.com',
