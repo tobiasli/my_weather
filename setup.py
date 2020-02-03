c24ee43d20b99ceeb44323b7f1d5b27f0761107e
Tobias Litherland <tobias.litherland@statkraft.com>
2020-02-03 21:13:30 +0100

    New version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='weather-tobiasli',
-                 version='1.1.0',
+                 version='1.1.1',
                  description='Tools for managing weather data using shyft.',
                  author='Tobias Litherland',
                  author_email='tobiaslland@gmail.com',
